d53434d3cdf1c22608164153efed718871cf071e
Kolya Efimov <999vaz00@mail.ru>
2026-02-23 19:40:30 +0300

    homework-16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,82 @@
 import { Component } from '@angular/core';
-import './training.ts';
 import { Color } from '../enums/Color';
+import { IService } from '../interfaces/IService.js';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import './training.ts';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  companyName: string = 'РУМТИБЕТ';
-
-  isPrimaryColor(color: Color): boolean {
-    const primaryColors: Color[] = [Color.RED, Color.GREEN, Color.BLUE];
-    return primaryColors.includes(color);
-  }
+  isLoading = true;
+  inputValue: string = '';
+  displayedText: string = '';
+  clickCount: number = 0;
+  buttonText: string = '';
+  showDate = true;
+  showClicker = false;
+  currentDate: string = '';
+  location: string = '';
+  date: string = '';
+  participants: string = '';
+  isButtonEnabled: boolean = false;
+  readonly companyName: string = 'РУМТИБЕТ';
+  services: IService[] = [
+    {
+      id: 1,
+      title: 'Опытный гид',
+      description:
+        'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
+      icon: 'people-icon',
+    },
+    {
+      id: 2,
+      title: 'Безопасный поход',
+      description:
+        'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
+      icon: 'shield-icon',
+    },
+    {
+      id: 3,
+      title: 'Лояльные цены',
+      description:
+        'Для современного мира базовый вектор развития предполагает независимые способы реализации соответствующих условий активизации.',
+      icon: 'label-icon',
+    },
+  ];
 
   constructor() {
     this.setLastVisitDate();
     this.setVisitCount();
+  }
+
+  onInit(): void {
+    this.checkFormValidity();
+  }
+
+  checkFormValidity(): void {
+    this.isButtonEnabled = this.location !== '' && this.date !== '' && this.participants !== '';
+  }
+
+  onLocationChange(): void {
+    this.checkFormValidity();
+  }
+
+  onDateChange(): void {
+    this.checkFormValidity();
+  }
+
+  onParticipantsChange(): void {
+    this.checkFormValidity();
+  }
+
+  isPrimaryColor(color: Color): boolean {
+    const primaryColors: Color[] = [Color.RED, Color.GREEN, Color.BLUE];
+    return primaryColors.includes(color);
   }
 
   setLastVisitDate(): void {
@@ -34,5 +92,42 @@ export class AppComponent {
     visitCount++;
     localStorage.setItem('visitCount', visitCount.toString());
     console.log('Количество заходов:', visitCount);
+  }
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.currentDate = new Date().toLocaleString();
+    }, 1000);
+  }
+
+  increment() {
+    this.clickCount++;
+  }
+
+  decrement() {
+    if (this.clickCount > 0) {
+      this.clickCount--;
+    }
+  }
+
+  toggleDisplay() {
+    this.showDate = !this.showDate;
+    this.showClicker = !this.showClicker;
+
+    if (this.showDate) {
+      this.buttonText = 'Показать кликер';
+    } else {
+      this.buttonText = 'Показать дату';
+    }
+  }
+
+  onInput() {
+    this.displayedText = this.inputValue;
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
   }
 }
