e6a9074d8ba6b55f555b8bb3872a544722612f9b
Kolya Efimov <999vaz00@mail.ru>
2026-06-16 13:49:12 +0300

    fix-error

--- src/app/logging.interceptor.ts
+++ src/app/logging.interceptor.ts
@@ -6,19 +6,19 @@ export const loggingInterceptor: HttpInterceptorFn = (req, next) => {
   const startTime: number = Date.now();
 
   return next(req).pipe(
-    tap((event: HttpEvent<any>) => {
+    tap((event: HttpEvent<unknown>) => {
       const endTime: number = Date.now();
       const duration: number = endTime - startTime;
 
       if (event instanceof HttpResponse) {
         console.log(
-          `Метод: ${req.method}` +
-          `URL: ${req.url}` +
-          `Статус: ${event.status}` +
-          `Время выполнения: ${duration}`,
+          `Метод: ${ req.method }` +
+          `URL: ${ req.url }` +
+          `Статус: ${ event.status }` +
+          `Время выполнения: ${ duration }`,
         );
       }
     }),
   );
   
-};
+}
